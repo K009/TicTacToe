d53c943699f60b1cb3cb854c9a75d84c58b0f38b
Kkk <kkk@example.com>
2020-03-23 21:58:47 +0100

    Field content can be changed error handler. Added error handler

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,18 @@
 game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0],]
-def game_board():
-    for count,row in enumerate(game):
-        print(row)
-'''game_board()'''
+
+
+def game_board(game_map, player = 0, row = 0, column = 0, jut_display = False):
+	try:
+		print("   0  1  2")
+		if not jut_display:
+			game_map[row][column] = player
+		for count, row in enumerate(game_map):
+			print(count, row)
+		return game_map
+	except:
+		print("Something went wrong!")
+
+game = game_board(game, jut_display = True)
+game = game_board(game, player = 1, row = 1, column = 1)
