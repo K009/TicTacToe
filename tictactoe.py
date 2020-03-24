0704ce220bf7fca76047ffcf228ef00488b720ed
Kkk <kkk@example.com>
2020-03-24 10:13:18 +0100

    Added basic console UI and iterators for players' turns

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,9 @@
-game = [[0, 1, 0],
-        [0, 0, 1],
-        [0, 1, 0],]
+import itertools
+
+
+game = [[0, 1, 1],
+        [0, 1, 1],
+        [1, 1, 0],]
 
 
 def game_board(game_map, player = 0, row = 0, column = 0, jut_display = False):
@@ -18,10 +21,13 @@ def game_board(game_map, player = 0, row = 0, column = 0, jut_display = False):
 
 
 def win(current_game):
+	# Horizontal
 	for row in game:
 		print(row)
 		if row.count(row[0]) == len(row) and row[0] != 0:
-			print("Winner")
+			print(f"Player {row[0]} is the winner horizontally.")
+
+	# Vertical
 	for col in range(len(game)):
 		check = []
 
@@ -31,17 +37,54 @@ def win(current_game):
 		if check.count(check[0]) == len(check) and check[0] != 0:
 			print("Winner")
 
+	# Diagonal
 	diags=[]
 	for col, row in enumerate(reversed(range(len(game)))):
 		diags.append(game[row][col])
-	print(diags)
+	if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+			print(f"Player {diags[0]} is the winner diagonally. (/)")
 
+	# Diagonal
 	diags=[]
 	for ix in range(len(game)):
 		diags.append(game[ix][ix])
 	print(diags)
 
+play = True
+players = [1,2]
+while play:
+	game = [[0, 0, 0],
+	        [0, 0, 0],
+	        [0, 0, 0]]
 
-game = game_board(game, jut_display = True)
-game = game_board(game, player = 1, row = 0, column = 0)
-win(game)
+	game_won = False
+	game = game_board(game, jut_display = True)
+	player_choice = itertools.cycle([1, 2])
+	while not game_won:
+		current_player = next(player_choice)
+		print(f"Current player {current_player}")
+		column_choice = int(input("Choose a column you wanna play (0, 1, 2): "))
+		row_choice = int(input("Choose a row you wanna play (0, 1, 2): "))
+		game = game_board(game, current_player, row_choice, column_choice)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
